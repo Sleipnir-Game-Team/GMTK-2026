052fef20f28c15c5c1bffc65cea896f61fe30acf
FMABr <felipemuamorim@gmail.com>
2026-07-24 23:55:55 -0400

    0.1.14 - move stuff around maybe

--- game/ship_modules/scripts/screw.gd
+++ game/ship_modules/scripts/screw.gd
@@ -11,8 +11,14 @@ func _ready() -> void:
 		remaining -= 1
 		screws[remaining].loose = state_manager.screw_states[screen_name][remaining]
 		screws[remaining].changed.connect(on_screw_changed.bind(remaining))
+		
+	if get_unscrewed() > 0:
+		AudioManager.play_global("screw.loose")
 
 func on_screw_changed(state: bool, screw: int) -> void:
+	if get_unscrewed() == 0:
+		AudioManager.stop_global("screw.loose")
+
 	state_manager.screw_states[screen_name][screw] = state
 
 func get_unscrewed() -> int:
--- game/uses/scripts/crack.gd
+++ game/uses/scripts/crack.gd
@@ -10,32 +10,8 @@ signal finished(enough: bool)
 var _taped: int = 0
 var _taping: bool = false:
 	set(taping):
-		if taping:
-			_current_tape = Line2D.new()
-			_current_tape.width = 30
-			_current_tape.texture = line_texture
-			_current_tape.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
-			_current_tape.texture_mode = Line2D.LINE_TEXTURE_TILE
-			_current_tape.add_point(get_local_mouse_position())
-			_current_tape.add_point(get_local_mouse_position())
-			add_child(_current_tape)
-		elif _taping: # was taping and is no longer
-			var query := PhysicsShapeQueryParameters2D.new()
-			query.collide_with_areas = true
-			query.collide_with_bodies = false
-			query.collision_mask = 1 << 14 - 1
-			query.shape = SegmentShape2D.new()
-			query.shape.a = _current_tape.points[0]
-			query.shape.b = _current_tape.points[1]
-			
-			var space := get_world_2d().direct_space_state
-			var results := space.intersect_shape(query)
-			for result in results:
-				var shape_owner = cracked_area.shape_find_owner(result.shape)
-				cracked_area.shape_owner_get_owner(shape_owner).queue_free()
-				_taped += 1
-			
-			finished.emit(_percentage_completed() > minimum_completion)
+		if not taping and _taping: # was taping and is no longer
+			stop_taping()
 		
 		_taping = taping
 
@@ -51,7 +27,6 @@ func _ready() -> void:
 	var rectangle := RectangleShape2D.new()
 	rectangle.size.x = area.size.x / float(segment_columns)
 	rectangle.size.y = area.size.y / float(segment_rows)
-	
 	for row in segment_rows:
 		for column in segment_columns:
 			var shape := CollisionShape2D.new()
@@ -60,6 +35,8 @@ func _ready() -> void:
 			shape.position.y = crack.position.y + (rectangle.size.y * column) + (rectangle.size.y / 2)
 			shape.shape = rectangle
 			cracked_area.add_child(shape)
+	
+	AudioManager.play_global("gas.leak")
 
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
@@ -72,6 +49,7 @@ func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> voi
 	
 	elif event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT and points == 2:
 		_taping = false
+		AudioManager.play_global("oxygen.tape")
 
 
 func _on_mouse_exited() -> void:
@@ -86,9 +64,41 @@ func start_taping() -> void:
 	# Wait a frame to make sure the first call to _on_input_frame
 	# comes before we change _taping to true
 	await get_tree().process_frame
+	
+	_current_tape = Line2D.new()
+	_current_tape.width = 30
+	_current_tape.texture = line_texture
+	_current_tape.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
+	_current_tape.texture_mode = Line2D.LINE_TEXTURE_TILE
+	_current_tape.add_point(get_local_mouse_position())
+	_current_tape.add_point(get_local_mouse_position())
+	add_child(_current_tape)
+	
 	_taping = true
 	await finished
 
+func stop_taping() -> void:
+	var query := PhysicsShapeQueryParameters2D.new()
+	query.collide_with_areas = true
+	query.collide_with_bodies = false
+	query.collision_mask = 1 << 14 - 1
+	query.shape = SegmentShape2D.new()
+	query.shape.a = _current_tape.points[0]
+	query.shape.b = _current_tape.points[1]
+	
+	var space := get_world_2d().direct_space_state
+	var results := space.intersect_shape(query)
+	for result in results:
+		var shape_owner: int = cracked_area.shape_find_owner(result.shape)
+		cracked_area.shape_owner_get_owner(shape_owner).queue_free()
+		_taped += 1
+	
+	var stopped: bool = _percentage_completed() > minimum_completion
+	
+	if stopped:
+		AudioManager.stop_global("gas.leak")
+	
+	finished.emit(stopped)
 
 func _percentage_completed() -> float:
 	return float(_taped) / float(segment_columns * segment_rows)
